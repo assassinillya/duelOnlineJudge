b05373fa9b3292a87fa90b7de281f76df77a5a4f
asily <1664962271@qq.com>
2024-11-08 17:17:36 +0800

    feat(security): 添加 AES 加密工具类并集成 Sa-Token
    - 新增 AESUtil 类，用于密码加密和验证
    - 在 application.yml 中添加公钥和私钥配置
    - 更新 BackendApplication 以启用组件扫描
    - 添加单元测试以验证 AES 加密功能
    - 删除 MD5WithSaltEncryption 类，使用更安全的 AES 加密
    - 在 pom.xml 中添加 sa-token依赖

---
+++ backend/src/main/java/com/asily/utils/AESUtil.java
@@ -0,0 +1,21 @@
+package com.asily.utils;
+
+
+import cn.dev33.satoken.secure.SaSecureUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AESUtil {
+
+    @Value("${ojKey.public}")
+    private String publicKey;
+
+    public String generatePwd(String pwd) {
+        return SaSecureUtil.aesEncrypt(publicKey, pwd);
+    }
+
+    public boolean checkPwd(String pwd, String pwd2) {
+        return SaSecureUtil.aesEncrypt(publicKey, pwd).equals(pwd2);
+    }
+}
--- backend/src/test/java/com/asily/BackendApplicationTests.java
+++ backend/src/test/java/com/asily/BackendApplicationTests.java
@@ -1,14 +1,28 @@
 package com.asily;
 
+import cn.dev33.satoken.secure.SaSecureUtil;
+import com.asily.utils.AESUtil;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendApplicationTests {
 
+	@Autowired
+	private AESUtil aesUtil;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		System.out.println(SaSecureUtil.rsaGenerateKeyPair());
 	}
 
+	@Test
+	void testPwd() {
+		String s = aesUtil.generatePwd("123456");
+		System.out.println(s);
+		System.out.println(aesUtil.checkPwd("123456", s));
+
+	}
 
 }
